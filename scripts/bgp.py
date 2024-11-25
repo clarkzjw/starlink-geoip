c66e6a8b628b7e76d2768e364bdc6e0409872f6b
Jinwei Zhao <i@jinwei.me>
2024-11-25 12:46:31 -0800

    add BGP announcement check from bgp.tools (#2)
    * downloading bgp tables from bgp.tools
    
    * add github actions ci

---
+++ scripts/bgp.py
@@ -0,0 +1,63 @@
+import os
+import httpx
+import datetime
+
+import pandas as pd
+
+from pathlib import Path
+
+
+STARLINK_ASN = [14593, 45700]
+
+DATA_DIR = os.getenv("DATA_DIR", "./starlink-geoip-data")
+
+
+def get_date() -> str:
+    now = datetime.datetime.now(tz=datetime.timezone.utc)
+    return now.strftime("%Y%m%d-%H%M")
+
+
+date = get_date()
+
+
+headers={"User-Agent": "Starlink GeoIP Database GitHub Actions CI (https://github.com/clarkzjw/starlink-geoip)"}
+
+
+def new_client():
+    return httpx.Client()
+
+
+def get_bgp_list():
+    client = new_client()
+
+    print("Downloading BGP announcement table from bgp.tools")
+    response = client.get("https://bgp.tools/table.jsonl", headers=headers)
+    with open(f"./table.jsonl", "wb") as f:
+        f.write(response.content)
+
+    jsonObj = pd.read_json(path_or_buf="./table.jsonl", lines=True)
+    count = 0
+    total = len(jsonObj)
+
+    list = []
+    for line in jsonObj.iterrows():
+        ASN = line[1]['ASN']
+        count += 1
+        if count % 10000 == 0:
+            print(f"Iterating {count} BGP entries, {count/total:.2%} done")
+
+        if ASN in STARLINK_ASN:
+            CIDR = line[1]['CIDR']
+            HITS = line[1]['Hits']
+            list.append(f"{CIDR}, {ASN}, {HITS}\n")
+
+    with open(Path(DATA_DIR).joinpath("bgp/starlink-bgp.csv"), "w") as f1:
+        with open(Path(DATA_DIR).joinpath("bgp/starlink-bgp-{}.csv".format(date)), "w") as f2:
+            f1.write("CIDR,ASN,Hits\n")
+            f2.write("CIDR,ASN,Hits\n")
+            f1.writelines(list)
+            f2.writelines(list)
+
+
+if __name__ == '__main__':
+    get_bgp_list()
