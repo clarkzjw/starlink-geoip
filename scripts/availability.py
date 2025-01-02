fb57befdb1d4f459e753f2fd7e20a390e239fab0
Jinwei Zhao <clarkzjw@uvic.ca>
2025-01-02 12:53:43 -0800

    classify geojson into different files

--- scripts/availability.py
+++ scripts/availability.py
@@ -3,6 +3,8 @@
 
 # https://api.starlink.com/public-files/availability-cells.pb
 
+# visualize geojson with https://geojson.io/
+
 import os
 import json
 import httpx
@@ -39,7 +41,36 @@ def convert():
         f.write(json.dumps(data, indent=4))
 
 
+def classify():
+    status_dict = {}
+    with open(Path(DATA_DIR).joinpath("availability/availability-cells.geojson"), "r") as f:
+        data = json.load(f)
+        for feature in data["features"]:
+            status = feature["properties"]["status"]
+            if "expected" in feature["properties"]:
+                status += " " + feature["properties"]["expected"]
+            if status not in status_dict:
+                status_dict[status] = []
+            status_dict[status].append(feature)
+
+    for status in status_dict.keys():
+        # faq
+        # test
+        # blacklisted
+        # waitlisted Expanding in 2025
+        # waitlisted Sold Out
+        # waitlisted Expanding in 2025
+        # waitlisted Service date is unknown at this time
+        filepath = Path(DATA_DIR).joinpath("availability/{}.geojson".format(status.replace(" ", "_")))
+        with open(filepath, "w") as f:
+            f.write(json.dumps({
+                "type": "FeatureCollection",
+                "features": [feature for feature in status_dict[status]]
+            }, indent=4))
+
+
 if __name__ == "__main__":
     ensure_dir()
     retrive_availability_cells()
     convert()
+    classify()
