283e95bfd1c8651b7757c27ef250368a33780459
Jinwei Zhao <clarkzjw@uvic.ca>
2025-01-24 13:48:21 -0800

    update pop info

--- map/process_map.py
+++ map/process_map.py
@@ -1,14 +1,12 @@
 import re
 import os
 import sys
-import csv
 import time
 import json
 import httpx
 import requests
 import geocoder
 import pycountry
-from pprint import pprint
 from pathlib import Path
 
 
@@ -25,29 +23,32 @@ def get_geoip_json() -> dict:
 
 
 def get_pop_list(geoipJson: dict):
-    with open(Path(GEOIP_MAP_DIR).joinpath("pop.json"), 'r+') as f:
+    with open(Path(GEOIP_MAP_DIR).joinpath("pop.json"), "r+") as f:
         pops = json.load(f)
         pop_code_list = [x["code"] for x in pops]
 
         for pop in geoipJson["pop_subnet_count"]:
             if re.match(r"customer\.[a-z0-9]+\.pop\.starlinkisp\.net\.", pop[0]):
-                pop_code = pop[0].split('.')[1]
+                pop_code = pop[0].split(".")[1]
                 if pop_code not in pop_code_list:
                     print(pop_code)
-                    pops.append({
-                        "code": pop_code,
-                        "dns": pop[0],
-                        "city": "",
-                        "country": "",
-                        "lat": 0,
-                        "lon": 0,
-                        "show": False
-                    })
+                    pops.append(
+                        {
+                            "code": pop_code,
+                            "dns": pop[0],
+                            "city": "",
+                            "country": "",
+                            "lat": 0,
+                            "lon": 0,
+                            "show": False,
+                        }
+                    )
         pops.sort(key=lambda x: x["code"])
 
         f.seek(0)
         json.dump(pops, f, indent=4)
         f.truncate()
+
 
 def convert_country_code(two_letter_code: str) -> str:
     country = pycountry.countries.get(alpha_2=two_letter_code)
@@ -63,13 +64,10 @@ def get_netfac_list():
         print("Please set GEOCODER_TOKEN in environment variable")
         sys.exit(1)
 
-    netfac_geojson = {
-        "type": "FeatureCollection",
-        "features": []
-    }
+    netfac_geojson = {"type": "FeatureCollection", "features": []}
 
     netfac_ids = []
-    peeringdb_client = httpx.Client(base_url='https://www.peeringdb.com/')
+    peeringdb_client = httpx.Client(base_url="https://www.peeringdb.com/")
     for netid in PEERINGDB_NET_ID:
         netfac_json = requests.get(NETFAC_JSON_TEMPLATE_URL.format(netid))
         netfac_json = json.loads(netfac_json.content)
@@ -82,28 +80,43 @@ def get_netfac_list():
             name = netfac_detail["data"][0]["name"]
             address = ""
             fac_id = netfac_detail["data"][0]["fac_id"]
-            lat, lon = netfac_detail["data"][0]["fac"]["latitude"], netfac_detail["data"][0]["fac"]["longitude"]
+            lat, lon = (
+                netfac_detail["data"][0]["fac"]["latitude"],
+                netfac_detail["data"][0]["fac"]["longitude"],
+            )
 
             if lat is None or lon is None:
-                address += ",".join([netfac_detail["data"][0]["fac"]["address1"], netfac_detail["data"][0]["fac"]["city"], netfac_detail["data"][0]["fac"]["country"]])
+                address += ",".join(
+                    [
+                        netfac_detail["data"][0]["fac"]["address1"],
+                        netfac_detail["data"][0]["fac"]["city"],
+                        netfac_detail["data"][0]["fac"]["country"],
+                    ]
+                )
                 g = geocoder.google(address, key=token)
                 lat, lon = g.json["lat"], g.json["lng"]
 
-            netfac_geojson["features"].append({
-                "type": "Feature",
-                "geometry": {
+            netfac_geojson["features"].append(
+                {
+                    "type": "Feature",
+                    "geometry": {
                         "type": "Point",
-                        "coordinates": [float(lon), float(lat)]
+                        "coordinates": [float(lon), float(lat)],
                     },
-                "properties": {
-                    "title": name,
-                    "name": name,
-                    "description": "https://www.peeringdb.com/fac/{}".format(fac_id),
+                    "properties": {
+                        "title": name,
+                        "name": name,
+                        "description": "https://www.peeringdb.com/fac/{}".format(
+                            fac_id
+                        ),
+                    },
                 }
-            })
+            )
 
     peeringdb_client.close()
-    json.dump(netfac_geojson, open(Path(GEOIP_MAP_DIR).joinpath("netfac.json"), "w"), indent=4)
+    json.dump(
+        netfac_geojson, open(Path(GEOIP_MAP_DIR).joinpath("netfac.json"), "w"), indent=4
+    )
 
 
 def get_city_list(geoipJson: dict):
@@ -112,10 +125,7 @@ def get_city_list(geoipJson: dict):
         print("Please set GEOCODER_TOKEN in environment variable")
         sys.exit(1)
 
-    city_json = {
-        "type": "FeatureCollection",
-        "features": []
-    }
+    city_json = {"type": "FeatureCollection", "features": []}
 
     geoipJson = geoipJson["valid"]
     for country in geoipJson:
@@ -128,7 +138,9 @@ def get_city_list(geoipJson: dict):
                 country_full = convert_country_code(country)
                 print("{}, {}, {}".format(country, state, city))
 
-                g = geocoder.google("{}, {}, {}".format(city, state, country_full), key=token)
+                g = geocoder.google(
+                    "{}, {}, {}".format(city, state, country_full), key=token
+                )
                 if g.json is None:
                     g = geocoder.google("{}, {}".format(city, country_full), key=token)
                     if g.json is None:
@@ -139,21 +151,25 @@ def get_city_list(geoipJson: dict):
                 for ip in geoipJson[country][state][city]["ips"]:
                     description += "{}<br>({})<br>".format(ip[0], ip[1])
 
-                city_json["features"].append({
-                    "type": "Feature",
-                    "geometry": {
+                city_json["features"].append(
+                    {
+                        "type": "Feature",
+                        "geometry": {
                             "type": "Point",
-                            "coordinates": [float(source_gps["lng"]), float(source_gps["lat"])]
+                            "coordinates": [
+                                float(source_gps["lng"]),
+                                float(source_gps["lat"]),
+                            ],
                         },
-                    "properties": {
-                        "title": city,
-                        "name": "{}, {}, {}".format(country, state, city),
-                        "description": description,
+                        "properties": {
+                            "title": city,
+                            "name": "{}, {}, {}".format(country, state, city),
+                            "description": description,
+                        },
                     }
-                })
+                )
 
     json.dump(city_json, open(Path(GEOIP_MAP_DIR).joinpath("city.json"), "w"), indent=4)
-
 
 
 if __name__ == "__main__":
