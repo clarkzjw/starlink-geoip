15be14c456550055cda30e3eb90c15afb2ed70f5
Jinwei Zhao <clarkzjw@uvic.ca>
2025-11-15 00:38:55 -0800

    update schedule

--- run.py
+++ run.py
@@ -1,5 +1,13 @@
 from datetime import datetime, timezone
 
+import bgp
+import peeringdb
+import atlas
+import map.process_map
+import monthly_latency_snapshot
+import availability
+import geoip_pop
+
 
 def run_jobs(now: datetime):
 
@@ -8,20 +16,12 @@ def run_jobs(now: datetime):
     hour = now.hour
     day = now.day
 
-    if day in [1, 7, 14, 21, 28]:
-        import monthly_latency_snapshot
-        import availability
-
-        print("Running monthly latency snapshot and availability zone refresh...")
-        monthly_latency_snapshot.get_latency_json()
-        print("Refreshing availability zones...")
-        availability.refresh_availability_zone()
-
     if hour == 0:
-        import bgp
-        import peeringdb
-        import atlas
-        import map.process_map
+        if day in [1, 7, 14, 21, 28]:
+            print("Running monthly latency snapshot and availability zone refresh...")
+            monthly_latency_snapshot.get_latency_json()
+            print("Refreshing availability zones...")
+            availability.refresh_availability_zone()
 
         print("Refreshing BGP")
         bgp.get_bgp_list()
@@ -29,13 +29,12 @@ def run_jobs(now: datetime):
         peeringdb.refresh_peeringdb()
         print("Refreshing Atlas probes")
         atlas.refresh_atlas_probes()
-        print("Refreshing GeoIP map data")
-        map.process_map.refresh_map()
-
-    import geoip_pop
 
     print("Refreshing GeoIP and POP data")
     geoip_pop.refresh_geoip_pop()
+
+    print("Refreshing GeoIP map data")
+    map.process_map.refresh_map()
 
 
 if __name__ == "__main__":
